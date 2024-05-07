35b955f2ba1ffb1a8c591d948caba8f8ef34bb6f
eduardo01322 <eduardo.errea32@gmail.com>
2024-05-07 15:50:43 -0300

    .

--- routes/api.php
+++ routes/api.php
@@ -4,13 +4,13 @@ use App\Http\Controllers\filmeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-route::post('cadastro', [filmeController::class, 'cadastroFilme']);
+route::post('filmes/cadastro', [filmeController::class, 'cadastroFilme']);
 route::get('listagem', [filmeController::class, 'retornarTodos']);
-route::get('pesquisar/titulo', [filmeController::class, 'pesquisarPorTitulo']);
-route::get('pesquisar/diretor', [filmeController::class, 'pesquisarPorDiretor']);
-route::get('pesquisar/genero', [filmeController::class, 'pesquisarPorGenero']);
-route::delete('delete/{id}', [filmeController::class, 'excluir']);
-route::put('update', [filmeController::class, 'update']);
+route::get('filmes/pesquisar/titulo', [filmeController::class, 'pesquisarPorTitulo']);
+route::get('filmes/pesquisar/diretor', [filmeController::class, 'pesquisarPorDiretor']);
+route::get('filmes/pesquisar/genero', [filmeController::class, 'pesquisarPorGenero']);
+route::delete('filmes/delete/{id}', [filmeController::class, 'excluir']);
+route::put('filmes/update', [filmeController::class, 'update']);
 
 
 
